f92d6518c729dad944bfa7b72eefa03304b57f19
Ruslan Alpyspaev <rusalpyss@gmail.com>
2022-12-19 13:18:51 +0300

    "Is's working!"

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'posts.apps.PostsConfig',  # Добавленная запись
+    'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
