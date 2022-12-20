382aa6c30e0e6847693de35e84b9cb8e4f366f87
Ruslan Alpyspaev <rusalpyss@gmail.com>
2022-12-21 00:20:13 +0300

    "Is's working!"

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 from typing import Final
 
-POSTS_PER_PAGE : Final [int] = 10
+
+COUNT_PAGE: Final[int] = 10
+
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:POSTS_PER_PAGE]
+    posts = Post.objects.order_by('-pub_date')[:COUNT_PAGE]
     context = {
         'posts': posts,
     }
@@ -14,7 +16,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:POSTS_PER_PAGE]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:COUNT_PAGE]
     context = {
         'group': group,
         'posts': posts,
