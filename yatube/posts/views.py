19bb79dd6b32bfc04c3ef8c2f9623a031977e6bd
Ruslan Alpyspaev <rusalpyss@gmail.com>
2022-12-21 10:29:37 +0300

    "Is's working!"

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
-from typing import Final
+import typing
 
 
-COUNT_PAGE: Final[int] = 10
+COUNT_PAGE: typing.Final[int] = 10
 
 
 def index(request):
